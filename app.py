05d8c583e11e83381c042548534e29b15c9f27a9
Misha <mishagrigoryan@gmail.com>
2024-05-13 23:50:14 -0400

    rework

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import threading
 import time
@@ -11,26 +11,50 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 fake = Faker()
 
+generator_thread = None
+
 def generate_transaction():
     while True:
         transaction = {
-            "user_id": fake.unique.random_int(min=1, max=100000),
-            "product_id": fake.unique.random_int(min=1, max=100000),
+            "user_id": fake.random_int(min=1, max=1000),
+            "product_id": fake.random_int(min=1, max=100),
             "amount": round(random.uniform(5.0, 500.0), 2),
             "timestamp": str(fake.date_time_this_year())
         }
         socketio.emit('newdata', {'data': json.dumps(transaction)}, namespace='/test')
-        time.sleep(random.uniform(0.1, 0.5))  
+        time.sleep(random.uniform(0.1, 0.5))
+        if threading.current_thread().stopped():
+            break
 
+class StoppableThread(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    app.logger.info("Client connected")
+@socketio.on('start_generate', namespace='/test')
+def start_generate(message):
+    global generator_thread
+    if generator_thread is None or not generator_thread.is_alive():
+        generator_thread = StoppableThread(target=generate_transaction)
+        generator_thread.start()
+    emit('response', {'data': 'Started Generating'})
+
+@socketio.on('stop_generate', namespace='/test')
+def stop_generate(message):
+    global generator_thread
+    if generator_thread is not None:
+        generator_thread.stop()
+    emit('response', {'data': 'Stopped Generating'})
 
 if __name__ == '__main__':
-    threading.Thread(target=generate_transaction).start()
     socketio.run(app, debug=True)
